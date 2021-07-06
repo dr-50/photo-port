f0ff8c0b2cf53ce0ea4dab287242e66fcef096bd
Devon <devonross50@yahoo.com>
2021-07-06 18:51:56 -0500

    end of chapter section

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/about';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
